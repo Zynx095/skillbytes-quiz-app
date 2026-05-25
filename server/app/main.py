0a2966fe82934bd54310c7ffa807a51ab16a6919
Yukith M Joseph <179663742+Zynx095@users.noreply.github.com>
2026-05-26 01:52:24 +0530

    downgraded python version for mongodb tls fix

--- server/app/main.py
+++ server/app/main.py
@@ -1,24 +1,31 @@
 from fastapi import FastAPI
+
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes.quiz_routes import router
+
 from app.routes.analytics_routes import router as analytics_router
-from app.routes.quiz_routes import router as quiz_router
 
 app = FastAPI()
 
 app.add_middleware(
+
     CORSMiddleware,
-    allow_origins=["*"],
+
+    allow_origins=[
+
+        "http://localhost:5173",
+
+        "https://skillbytes-quiz-app.vercel.app/"
+    ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
-    allow_headers=["*"],
+
+    allow_headers=["*"]
 )
 
-app.include_router(quiz_router)
+app.include_router(router)
+
 app.include_router(analytics_router)
-
-@app.get("/")
-async def root():
-
-    return {
-        "message": "Database connected successfully"
-    }
