d88a8723c846925f7500b2c02f882062507d49f5
Yukith M Joseph <179663742+Zynx095@users.noreply.github.com>
2026-05-26 02:41:25 +0530

    changed baseURL in axios file

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
 
     baseURL:
-        "skillbytes-quiz-app-production.up.railway.app"
+        "skillbytes-quiz-app-production-19a9.up.railway.app"
 });
